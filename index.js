39d357e94332e14d0500d3a1ebe845b9ed50c63c
Zmulvihill <zmulvihi@yahoo.com>
2023-07-07 14:25:25 -0700

    added more styles, added function to display, all clear, and clear, still having issues with functions, saved, will pick up later

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 function App (){
 
     const [expression, setExpression] = React.useState("");
+    const [answer, setAnswer] = React.useState(0);
 
-    const display = () => {
-        setExpression(prev => prev + symbol)
+    const display = (symbol) => {
+        setExpression(prev => prev + symbol);
+        if (expression[expression.length - 1] == "=") {
+            if [/[0-9.]/.test(symbol)] {
+            setExpression(symbol);
+        } else {
+            setExpression(answer + symbol):
+        }
+        }
     };
 
+    const calculate = () => {
+        setAnswer(eval(expression));
+        setExpression((prev) => prev + "=");
+    };
+
+    const allClear = () => {
+        setExpression("");
+        setAnswer(0)
+    };
+    const clear = () => {
+        setExpression{(prev) =>
+         prev
+            .split("")
+            .splice(0, prev.length - 1)
+            .join("")
+        };
+        setAnswer(0);
+    }
     return (
         <div className="container">
             <div className="grid">
                 <div className="dis">
-                <input type="text" value={expression} placeholder="0" disabled />
+                    <input type="text" value={expression} placeholder="0" disabled />
+                    <div className="total">(answer)</div>
                 </div>
-                <div onClick={display} className="dis"></div>
-                <div onClick={display} className="padButton AC tomato">AC</div>
-                <div onClick={display} className="padButton C tomato">C</div>
+                <div onClick={allClear} className="padButton AC tomato">AC</div>
+                <div onClick={clear} className="padButton C tomato">C</div>
                 <div onClick={() => display ("/")} className="padButton div">/</div>
-                <div onClick={() => display ("x")} className="padButton times">x</div>
+                <div onClick={() => display ("*")} className="padButton times">x</div>
                 <div onClick={() => display ("7")} className="padButton seven dark-gray">7</div>
                 <div onClick={() => display ("8")} className="padButton eight dark-gray">8</div>
                 <div onClick={() => display ("9")} className="padButton nine dark-gray">9</div>
@@ -28,7 +54,7 @@ function App (){
                 <div onClick={() => display ("1")} className="padButton one dark-gray">1</div>
                 <div onClick={() => display ("2")} className="padButton two dark-gray">2</div>
                 <div onClick={() => display ("3")} className="padButton three dark-gray">3</div>
-                <div onClick={display} className="padButton equal blue">=</div>
+                <div onClick={calculate} className="padButton equal blue">=</div>
                 <div onClick={() => display ("0")} className="padButton zero dark-gray">0</div>
                 <div onClick={() => display (".")} className="padButton dot dark-gray">.</div>
             </div>
